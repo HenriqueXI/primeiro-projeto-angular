c8dac87e5eefcccc14afce0b082b8baf689c0c40
Henrique <henriquedellosso@gmail.com>
2021-04-05 21:21:26 -0300

    primeiro-projeto-Angular

---
+++ src/app/products.ts
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    id: 1,
+    name: "Phone XL",
+    price: 799,
+    description: "um celular largo"
+  },
+  {
+    id: 2,
+    name: "Phone Mini",
+    price: 699,
+    description: "Um otimo celular com uma boa camera"
+  },
+  {
+    id: 3,
+    name: "Phone Standard",
+    price: 299,
+    description: ""
+  },
+  {
+    id: 4,
+    name: "JBL",
+    price: 800,
+    description: "A melhor caixa de som que você já viu!!"
+  }
+];
